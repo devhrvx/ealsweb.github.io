1403d7748b162b06e0ee2cca67bf7058a0ea90ff
hrvyCased <salinas.johnharvey11@gmail.com>
2024-03-09 19:38:47 +0800

    Created first kayout, styles, and script test for button.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var exploreBtn = document.querySelector('#explore-btn');
+    exploreBtn.addEventListener('click', function() {   
+        window.location.href = 'https://www.google.com';
+    });
+});
