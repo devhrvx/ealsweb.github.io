7481b6eb7466bf1d6674ac129af3152b9b25ddf1
hrvyCased <salinas.johnharvey11@gmail.com>
2024-03-10 12:28:29 +0800

    update

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var exploreBtn = document.getElementById('explore-btn');
-    function scrollSmooth(y) {
-        window.scrollTo({
-            top: y,
-            behavior: 'smooth'
-        });
-    }
     exploreBtn.addEventListener('click', function() {
-        scrollSmooth(672);
-    });
-    var nextBtn = document.getElementById('next-btn');
-    nextBtn.addEventListener('click', function() {
         window.location.href = 'universe.html';
     });
 });
