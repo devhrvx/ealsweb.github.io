3d02e2ce0ac1b316ad5e1cefa654a368587de76f
hrvyCased <salinas.johnharvey11@gmail.com>
2024-03-20 20:58:22 +0800

    Update image sources and add click event listeners

--- universe.js
+++ universe.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var pics1 = ["steady-state", "milky"];
-    var pics2 = ["Untitled design (2)", "steady-state"];
-    var imgContent = document.querySelector("#img-content");
-    var imgContent2 = document.querySelector("#img-content2");
-    var box2Img = document.querySelector(".box2");
-    var box3Img = document.querySelector(".box3");
-    box2Img.addEventListener('click', function() {
-        changeImg(imgContent, ...pics1);
-    });
-    box3Img.addEventListener('click', function() {
-        changeImg(imgContent2, ...pics2);
-    });
-    function changeImg(imgCon, ...pics) {
-        console.log("Box 2 clicked");
-        console.log("Current image source:", imgCon.src);
-        if (imgCon.src.includes(pics[0] + ".jpg")) {
-            console.log("Changing image to", pics[1]);
-            imgCon.src = pics[1] + ".jpg";
+
+    var imageContent1 = document.querySelector('#img-content');
+    var imageContent2 = document.querySelector('#img-content2');
+    var currentState = true;
+
+    imageContent1.addEventListener('click', function() {
+        if (currentState) {
+            changePicture(imageContent1, 'steady-state.jpg');
         } else {
-            console.log("Changing image to", pics[0]);
-            imgCon.src = pics[0] + ".jpg";
+            changePicture(imageContent1, 'milky.jpg');
         }
+        currentState = !currentState;
+    });
+
+    imageContent2.addEventListener('click', function() {
+        if (currentState) {
+            changePicture(imageContent2, 'Untitled design (2).png');
+        } else {
+            changePicture(imageContent2, 'milky.jpg');
+        }
+        currentState = !currentState;
+    });
+
+    function changePicture(image, newImageSrc) {
+        image.src = newImageSrc;
     }
 });
