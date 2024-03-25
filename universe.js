bdffdbe0a9be1ffeefeda7722d2b6cb31f8b2e1f
hrvyCased <salinas.johnharvey11@gmail.com>
2024-03-25 11:24:26 +0800

    Update universe.css and universe.html

--- universe.js
+++ universe.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     var imageContent1 = document.querySelector('#img-content');
+    var imageContent2 = document.querySelector('#img-content2');
+    var currentState2 = true;
     var currentState = true;
 
     imageContent1.addEventListener('click', function() {
         if (currentState) {
             changePicture(imageContent1, 'images/steady-state.jpg');
         } else {
-            changePicture(imageContent1, 'images/milky.jpg');
+            changePicture(imageContent1, 'images/quasar.jpg');
         }
         currentState = !currentState;
     });
 
+    imageContent2.addEventListener('click', function() {
+        if (currentState2) {
+            changePicture(imageContent2, 'images/milky.jpg');
+        } else {
+            changePicture(imageContent2, 'images/uranus.png');
+        }
+        currentState2 = !currentState2;
+    });
     function changePicture(image, newImageSrc) {
         image.src = newImageSrc;
     }
