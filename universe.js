9d56504bc0bcc3cd4f0b4478c7d3f339caa9b4cb
hrvyCased <salinas.johnharvey11@gmail.com>
2024-03-27 12:08:21 +0800

    Add new container and content for Big Bang Theory

--- universe.js
+++ universe.js
@@ -2,8 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var imageContent1 = document.querySelector('#img-content');
     var imageContent2 = document.querySelector('#img-content2');
+    var imageContent3 = document.querySelector('#img-content3');
     var currentState2 = true;
     var currentState = true;
+    var currentState3 = true;
+
+    imageContent3.addEventListener('click', function() {
+        if (currentState3) {
+            changePicture(imageContent3, 'images/earth.png');
+        } else {
+            changePicture(imageContent3, 'images/bang.jpg');
+        }
+        currentState3 = !currentState3;
+    });
 
     imageContent1.addEventListener('click', function() {
         if (currentState) {
