cfbb7d6d54f0481e4f8e29377fee18f72a3877e5
hrvyCased <salinas.johnharvey11@gmail.com>
2024-04-04 20:33:46 +0800

    Update explore button link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var exploreBtn = document.getElementById('explore-btn');
     exploreBtn.addEventListener('click', function() {
-        window.location.href = 'universe.html';
+        window.location.href = 'select.html';
     });
 });
