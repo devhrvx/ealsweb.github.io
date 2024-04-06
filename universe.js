5c5c42b3257f22158af8a1b8392b9b125ba04977
hrvyCased <salinas.johnharvey11@gmail.com>
2024-04-06 16:15:56 +0800

    Add new images and update styles

--- universe.js
+++ universe.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var imageContent1 = document.querySelector('#img-content');
     var imageContent2 = document.querySelector('#img-content2');
     var imageContent3 = document.querySelector('#img-content3');
+    var imageContent4 = document.querySelector('#img-content4');
+    var currentState4 = true;
     var currentState2 = true;
     var currentState = true;
     var currentState3 = true;
@@ -25,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         currentState = !currentState;
     });
 
+    
     imageContent2.addEventListener('click', function() {
         if (currentState2) {
             changePicture(imageContent2, 'images/milky.jpg');
@@ -32,6 +35,14 @@ document.addEventListener('DOMContentLoaded', function() {
             changePicture(imageContent2, 'images/pulsate.jpg');
         }
         currentState2 = !currentState2;
+    });
+    imageContent4.addEventListener('click', function() {
+        if (currentState4) {
+            changePicture(imageContent4, 'images/cmb1.jpg');
+        } else {
+            changePicture(imageContent4, 'images/CMB2.jpg');
+        }
+        currentState4 = !currentState4;
     });
     function changePicture(image, newImageSrc) {
         image.src = newImageSrc;
